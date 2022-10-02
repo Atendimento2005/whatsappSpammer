7d88b50f1e77833330c471a59a709d7184321732
Atendimento2005 <gogo.june2005@gmail.com>
2022-10-02 12:15:35 +0530

    Updated!

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from cgitb import text
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,28 +10,19 @@ driver.get('https://web.whatsapp.com')
 
 wait = WebDriverWait(driver, 100)
 
+message = '''
+Test Message OP
+'''
+
 with open('links.txt', 'r+') as f:
 	links = f.readlines()
 	for link in links:
 		driver.get(link)
-		textField = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '._1LbR4 > div:nth-child(2)')))
+		textField = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.fd365im1')))
 
 		textField.clear()
-		textField.send_keys(
-'''
-Hi,
-We're grateful for your demonstrated interest in our program, and we're happy to tell you that you've been offered a place in the Summer Camp, organised by DPSRPK's Tech Club!
-
-Included in this email are two links, one for the WhatsApp group, and the other for the Discord server. All members are required to join the groups on both platforms. If you haven't used Discord before, you could use the following YouTube video to familiarize yourself with the application:
-https://www.youtube.com/watch?v=nPmdafMo1b8
-
-Moreover, we share your enthusiasm for the program, and we're looking forward to meeting you!
-
-Discord Server Invite: https://discord.gg/fnWq9TdZwz
-
-Whatsapp Group Invite: https://chat.whatsapp.com/G7ytufqES1Q2BsddWiyqQP
-'''
-		)
+		for i in message:
+			textField.send_keys(i)
 		sleep(1)
 		textField.send_keys(Keys.RETURN)
 		print(link)
